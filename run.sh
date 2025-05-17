59f925f781f4879d7916641c571de5e9bb56c039
katamar1 <149537667+katamar1@users.noreply.github.com>
2025-05-17 16:21:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Exit if any command fails
 set -e
-
-# Build the project
 cmake -S . -B build
 cmake --build build
-
-# Run the server from the project root so it can access ./public
 ./build/http_server
